af06027931de7c1f744e8c1fb1464f0775b29bdf
Przemyslaw Przyslo <przemyslaw.przyslo.500@guest.p.lodz.pl>
2017-07-19 20:23:28 +0200

    kopia przed zmianą logowania z FileRealm na JDBCRealm

--- src/main/java/pl/turek/liceum/rentit/model/ReservStatus.java
+++ src/main/java/pl/turek/liceum/rentit/model/ReservStatus.java
@@ -10,11 +10,14 @@ import java.util.Collection;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -26,6 +29,7 @@ import javax.xml.bind.annotation.XmlTransient;
  */
 @Entity
 @Table(name = "RESERV_STATUS", catalog = "", schema = "RENTIT")
+@TableGenerator(name = "Reserv_statusIdGen", table = "GENERATOR", pkColumnName = "ENTITY_NAME", valueColumnName = "ID_RANGE", pkColumnValue = "Reserv_status",allocationSize = 1, initialValue=10)
 @XmlRootElement
 @NamedQueries({
     @NamedQuery(name = "ReservStatus.findAll", query = "SELECT r FROM ReservStatus r")
@@ -38,6 +42,7 @@ public class ReservStatus implements Serializable {
     @Basic(optional = false)
     @NotNull
     @Column(name = "ID", nullable = false)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "Reserv_statusIdGen")
     private Integer id;
     @Size(max = 255)
     @Column(name = "RESERVATION_STATUS_NAME", length = 255, nullable = false)
